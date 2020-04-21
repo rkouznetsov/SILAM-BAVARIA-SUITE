b19ae07d2fcc8e16b0b0c1fdb6bd243457d16ea8
Rostislav Kouznetsov <rostoslav.kouznetsov@fmi.fi>
2020-04-21 16:10:06 +0300

    Boundary downloader

---
+++ get_pollen_BND.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Script to download silam european pollen runs from FMI SILAM thredds setup
+# (c)opyleft Roux aka Rostislav DOT Kouznetsov AT fmi.fi 
+#Updated  for v5_5 Jun 2020
+
+# Enjoy!
+
+email="dimiter.syrakov@test-k.teho-login1"
+email="test-download-bavaria"
+#email=test
+
+set -e
+
+if [ -z "$1" ]; then # get the argument (does not make much sense since only yesterdays data are available)
+     basedate=yesterday
+else 
+     basedate=$1
+fi
+set -u 
+ncks=ncks
+
+BND_PATH=SILAM-bnd
+
+targetdir=$BND_PATH/`date -u -d "$basedate" +%Y%m%d00`
+mkdir -p $targetdir
+cd $targetdir
+
+
+
+#echo $SHELL
+#exit 1
+
+runpref=silam_europe_pollen_v5_7-TopSecret_RUN_
+urlbase="http://silam.fmi.fi/thredds/ncss/silam_europe_pollen_v5_7-TopSecret/runs/$runpref"
+
+pollens="POLLEN_ALDER_m22 POLLEN_BIRCH_m22 POLLEN_GRASS_m32 POLLEN_MUGWORT_m18 POLLEN_OLIVE_m28 POLLEN_RAGWEED_m18"
+
+pollenvars="Poll_Rdy2fly heatsum poll_left poll_tot_m2 pollen_corr  cnc"
+varlist="daymean_temp2m,temp_2m_acc"
+
+for pol in $pollens; do
+  for var in $pollenvars; do
+    varlist="$varlist,${var}_${pol}"
+  done
+done
+
+echo $varlist
+#exit 0
+
+
+
+
+
+# German domain
+bbox="spatial=bb&north=56&west=4&east=16&south=46"
+
+
+
+maxjobs=2
+
+# make dates
+run=`date -u -d $basedate +"%FT00:00:00Z"`
+
+
+
+for try  in `seq 0 10`; do
+   missfiles=""
+   #for hr in `seq 24 72` ; do
+   for hr in `seq 24` ; do
+        time=`date -u -d"$basedate + $hr hours" +"%FT%H:00:00Z"`
+        outf=`date -u -d"$basedate + $hr hours" +"SILAM4DE${run}_%Y%m%d%H.nc"`
+        [ -f $outf ] && continue
+         missfiles="$missfiles $outf"
+
+
+        URL="$urlbase$run?var=$varlist&$bbox&temporal=range&time_start=$time&time_end=$time&accept=netcdf&email=$email"
+        (wget --progress=dot:giga $URL -O ${outf}.tmp && $ncks --mk_rec_dmn time ${outf}.tmp $outf && rm ${outf}.tmp ) &
+        while [ `jobs | wc -l` -ge $maxjobs ]; do sleep 1; done
+
+   done
+   wait
+   [ -z "$missfiles" ] && break
+done
+
+
+if [ -z "$missfiles" ]; then
+  echo "Finished okay after $try attempts"
+  exit 0
+else
+  echo Failed!
+  exit 255
+fi
+
+
