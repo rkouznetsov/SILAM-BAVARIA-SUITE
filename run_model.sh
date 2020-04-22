ab460a7a394a302a83df42aac2c9b80f3fdc6674
Rostislav Kouznetsov <rostoslav.kouznetsov@fmi.fi>
2020-04-22 15:18:40 +0300

    Control and supplementary files
    technically-working setup

--- run_model.sh
+++ run_model.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -u
 set -e
 set -o pipefail
@@ -7,8 +7,10 @@ START_TIME=`date -u -d $fcdate +"%Y %m %d 00 00 0."`
 export START_TIME
 
 # mpirun -n $ny $silam_binary $scriptdir/MoscowICON.ctrl 2>&1 | tee $outputdir/${fcdate}/outlog_`date -u +"%Y%m%d%H%M%S"`.log
-mkdir -p $outputdir/${fcdate}
-$silam_binary $scriptdir/MoscowICON.ctrl 2>&1 | tee $outputdir/${fcdate}/outlog_`date -u +"%Y%m%d%H%M%S"`.log
+mkdir -p $OUTPUTDIR/${fcdate}
+/usr/bin/time -v $silam_binary $scriptdir/BavarianPollen.ctrl 2>&1 | tee $OUTPUTDIR/${fcdate}/outlog_`date -u +"%Y%m%d%H%M%S"`.log
+OMP_NUM_THREADS=1
+#gdb -ex 'break globals::set_error'  -ex 'break globals::ooops' -ex r --args ${silam_binary}_debug $scriptdir/BavarianPollenDebug.ctrl
 
 
 
