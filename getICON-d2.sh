8a193adec2a5ac9406a2dc30bf4b91b8a1d8f34c
Rostislav Kouznetsov <rostislav.kouznetsov@fmi.fi>
2021-02-11 23:41:46 +0200

    Icon d2 meteo download
    (cosmo is dead)

--- getICON-d2.sh
+++ getICON-d2.sh
@@ -29,18 +29,17 @@ fcdate=`echo $fcdate |cut -b 1-8`${fch}
 
 
 echo  fcdate=$fcdate
+#https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/alb_rad/icon-d2_germany_regular-lat-lon_single-level_2021021103_000_2d_alb_rad.grib2.bz2
+
+urlpref=https://opendata.dwd.de/weather/nwp/icon-d2/grib/${fch}
 
 
-urlpref=https://opendata.dwd.de/weather/nwp/cosmo-d2/grib/${fch}
-
-
-
-outdir=$METEO_DIR/$fcdate
+outdir=$ICON_PATH/$fcdate
 mkdir -p $outdir
 
 
 ##working directory for temporary files
-scratchdir=/dev/shm/getCOSMO${fcdate}
+scratchdir=/tmp/getICON${fcdate}
 rm -rf $scratchdir
 mkdir -p $scratchdir
 cd $scratchdir
@@ -52,9 +51,9 @@ ncurls=60
 var3d="clc p qv t tke u v w"
 
 
-staticvar="fc hsurf fis fr_land soiltyp plcov rootdp"
+staticvar="fr_lake fr_land fr_ice hsurf soiltyp plcov lai"
 staticvarnn="soiltyp"
-staticnotuse="rlat rlon"
+staticnotuse="elat elon clat clon"
 
 static3d="hhl"
 
@@ -66,9 +65,9 @@ relhum  # Rh @pressure level
 "
 
 #varsfc="alb_rad alhfl_s asob_s asob_t aswdifd_s aswdifu_s aswdir_s cape_con cape_ml clch clcl clcm clct clct_mod cldepth fr_ice h_snow hbas_con htop_con htop_dc hzerocl pmsl ps qv_s rain_con rain_gsp relhum_2m rho_snow snow_con snow_gsp t_2m t_g t_snow td_2m tmax_2m tmin_2m tot_prec u_10m v_10m vmax_10m w_snow ww z0 "
-#varsfc="alb_rad alhfl_s asob_s asob_t aswdifd_s aswdifu_s aswdir_s cape_con cape_ml clch clcl clcm clct clct_mod cldepth h_snow hbas_con htop_con htop_dc hzerocl pmsl ps qv_s rain_con rain_gsp relhum_2m rho_snow snow_con snow_gsp t_2m t_g t_snow td_2m tmax_2m tmin_2m tot_prec u_10m v_10m vmax_10m w_snow ww z0 "
+varsfc="alb_rad alhfl_s asob_s asob_t aswdifd_s aswdifu_s aswdir_s cape_ml clch clcl clcm clct clct_mod cldepth h_snow hbas_sc htop_sc htop_dc hzerocl pmsl ps qv_s rain_con rain_gsp relhum_2m rho_snow snow_con snow_gsp t_2m t_g t_snow td_2m tmax_2m tmin_2m tot_prec u_10m v_10m vmax_10m w_snow ww z0 "
 
-varsfc="alb_rad alhfl_s ashfl_s asob_s asob_t aswdifd_s aswdifu_s aswdir_s cape_ml cin_ml  clch clcl clcm clct clct_mod cldepth dbz_850 dbz_cmax  grau_gsp h_snow hzerocl  mh  pmsl prs_gsp ps  qv_s relhum_2m rho_snow runoff_g runoff_s sdi_1 sdi_2 snow_gsp snowlmt t_2m t_g t_s t_snow  tch tcm td_2m tot_prec u_10m v_10m w_snow  ww z0"
+#varsfc="alb_rad alhfl_s ashfl_s asob_s asob_t aswdifd_s aswdifu_s aswdir_s cape_ml cin_ml  clch clcl clcm clct clct_mod cldepth dbz_850 dbz_cmax  grau_gsp h_snow hzerocl  mh  pmsl prs_gsp ps  qv_s relhum_2m rho_snow rootdp runoff_g runoff_s sdi_1 sdi_2 snow_gsp snowlmt t_2m t_g t_s t_snow  tch tcm td_2m tot_prec u_10m v_10m w_snow  ww z0"
 
 varsoil=""
 for soillev in 0 1 3 9 27 81 729; do ###Layers in soil
@@ -78,11 +77,12 @@ for soillev in 0 2 6 18 54 162 486 1458; do  ##levels in soil
   varsoil="$varsoil ${soillev}_t_so"
 done
 
-pref_static=cosmo-d2_germany_rotated-lat-lon_time-invariant_
-pref_3d=cosmo-d2_germany_rotated-lat-lon_model-level_
-pref_3dh=cosmo-d2_germany_rotated-lat-lon_hybrid-level_
-pref_sfc=cosmo-d2_germany_rotated-lat-lon_single-level_
-pref_soil=cosmo-d2_germany_rotated-lat-lon_soil-level_
+
+pref_static=icon-d2_germany_regular-lat-lon_time-invariant_
+pref_3d=icon-d2_germany_regular-lat-lon_model-level_
+pref_3dh=icon-d2_germany_regular-lat-lon_hybrid-level_
+pref_sfc=icon-d2_germany_regular-lat-lon_single-level_
+pref_soil=icon-d2_germany_regular-lat-lon_soil-level_
 #cutgrib="cdo sellonlatbox,32.2,43,50.2,62" 
 
 #export https_proxy=http://wwwproxy.fmi.fi:8080
@@ -93,15 +93,18 @@ staticfile=$outdir/${filepref}${fcdate}.grib2
 if [ ! -f $staticfile ]; then
   for itry in `seq 1 10`; do
     for v in $staticvar; do
-      V=`echo $v | tr  '[:lower:]' '[:upper:]'`
-      destfile=${filepref}${fcdate}_${V}.grib2.bz2
+      V=$v #`echo $v | tr  '[:lower:]' '[:upper:]'`
+      destfile=${filepref}${fcdate}_000_0_${V}.grib2.bz2
       [ -s $destfile  ] && continue
+
       echo "curl -s -f --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 70 $urlpref/$v/$destfile -o ${destfile}.tmp && mv ${destfile}.tmp ${destfile} && echo ${destfile} Done, try $itry || echo ${destfile} Failed, try $itry.."
-      #curl --verbose   -f $urlpref/$v/$destfile -o $destfile
+      >&2 echo "curl --verbose   -f $urlpref/$v/$destfile -o $destfile"
     done |xargs -P $ncurls  -I XXX sh -c "XXX"
     base=`basename $staticfile`
     nmsg=`ls ${filepref}${fcdate}_*.grib2.bz2| wc -w` 
-    if [ $nmsg =  7 ]; then
+#https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/plcov/icon-d2_germany_regular-lat-lon_time-invariant_2021021103_000_0_plcov.grib2.bz2
+#https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/plcov/icon-d2_germany_regular-lat-lon_time-invariant_2021021103_plcov.grib2.bz2
+    if [ $nmsg ==  7 ]; then
       break
     else
       echo  "nfiles =  $nmsg Retry $itry"
@@ -124,28 +127,33 @@ if [ ! -f $staticfile ]; then
 fi
 
 
-
 steplist="`seq -f %03.0f 1 45`"
 #steplist="`seq -f %03.0f 1 12`"
 #steplist="001 002 003 "
 
 for step in $steplist; do
   file3dh=$outdir/${pref_3dh}${fcdate}+${step}.grib2 #final file in hybrid
-  if [ ! -f $file3dh ]; then
+  file3d=$outdir/${pref_3d}${fcdate}+${step}.grib2
+  #if [ ! -f $file3dh ]; then ## FIXME
+  if [ ! -f $file3d ]; then
     for itry in `seq 1 10`; do
       for v in $var3d; do
-        V=`echo $v | tr  '[:lower:]' '[:upper:]'`
-        for lev in `seq 1 65`; do
+        V=$v #`echo $v | tr  '[:lower:]' '[:upper:]'`
+#        https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/u/icon-d2_germany_regular-lat-lon_model-level_2021021103_001_44_U.grib2.bz2
+
+#        https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/u/icon-d2_germany_regular-lat-lon_model-level_2021021103_010_3_u.grib2.bz2
+        maxlev=65
+        [ "$V" == "w" ] && maxlev=66
+        for lev in `seq 1 $maxlev`; do
           destfile=${pref_3d}${fcdate}_${step}_${lev}_${V}.grib2.bz2
           [ -s $destfile  ] && continue
           echo "curl -s --max-time 40 -f $urlpref/$v/$destfile -o ${destfile}.tmp && mv ${destfile}.tmp ${destfile} && echo ${destfile} Done, try $itry || echo ${destfile} Failed, try $itry.."
         done
-      done | xargs -P $ncurls  -I XXX sh -c "XXX" || echo Some curls for $file3dh failed
+      done | xargs -P $ncurls  -I XXX -t sh -c "XXX" || echo Some curls for $file3dh failed
       
-      file3d=${pref_3d}${fcdate}+${step}.grib2
       base=`basename $file3dh`
       nmsg=`ls ${pref_3d}${fcdate}_${step}_*.grib2.bz2|wc -w`
-      if [ $nmsg ==  520 ]; then
+      if [ $nmsg ==  521 ]; then
         break
       else
         echo  "nmsg =  $nmsg Retry $itry"
@@ -158,10 +166,12 @@ for step in $steplist; do
       echo "$msg " | mailx -s "getCOSMO.sh failed  for Bavaria at Haze"  $MAILTO
       exit 255
     fi
-    cat ${pref_3d}${fcdate}_${step}_*.grib2.bz2 | lbzip2 -dvc > $file3d 
+    cat ${pref_3d}${fcdate}_${step}_*.grib2.bz2 | lbzip2 -dvc > $file3d.tmp
+    mv $file3d.tmp $file3d
     rm ${pref_3d}${fcdate}_*.grib2.bz2
-    $scriptdir/tools/COSMO-d2-2hybrid $file3d $base
-    mv $base $file3dh
+    ## FIXME
+    #$scriptdir/tools/COSMO-d2-2hybrid $file3d $base
+    #mv $base $file3dh
   fi
 
   filepref=$pref_sfc
@@ -169,18 +179,17 @@ for step in $steplist; do
   if [ ! -f $filesfc ]; then
     for itry in `seq 1 10`; do
       for v in $varsfc; do
-        V=`echo $v | tr  '[:lower:]' '[:upper:]'`
-          destfile=${filepref}${fcdate}_${step}_${V}.grib2.bz2
+        V=$v #`echo $v | tr  '[:lower:]' '[:upper:]'`
+          destfile=${filepref}${fcdate}_${step}_2d_${V}.grib2.bz2
           [ -f $destfile ] || echo "curl -s -f --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 70 $urlpref/$v/$destfile -o $destfile.tmp && mv ${destfile}.tmp ${destfile}"
       done | xargs -P $ncurls -t -I XXX sh -c "XXX" || echo Some curls for $filesfc failed
       base=`basename $filesfc`
       nmsg=`ls ${filepref}${fcdate}_${step}_*.grib2.bz2|wc -w`
-      if [ $nmsg ==  47 ]; then
+      if [ $nmsg ==  41 ]; then
         break
       else
         echo  "nmsg_sfc =  $nmsg Retry $itry"
       fi
-
       # nmsg=`grib_ls $file3d.tmp |grep "total messages" |awk '{print $1;}'`
     done
     if [ $itry -ge 9 ]; then
@@ -189,11 +198,15 @@ for step in $steplist; do
       echo "$msg " | mailx -s "getCOSMO.sh failed  for Bavaria at Haze"  $MAILTO
       exit 255
     fi
-    cat  ${filepref}${fcdate}_*.grib2.bz2 | lbzip2 -dc > $base.tmp 
+    cat  ${filepref}${fcdate}_*.grib2.bz2 | lbzip2 -dvc > $base.tmp 
     rm ${filepref}${fcdate}_*.grib2.bz2
     # get rid of 15-min steps
-    grib_set -w stepRange=${step} -S -s packingType=grid_ccsds $base.tmp $base
-    rm $base.tmp
+    stepmin=`expr ${step} \* 60`
+
+    grib_set -w stepRange=${stepmin} -S -s packingType=grid_ccsds $base.tmp $base
+    grib_set -w stepRange=0-${stepmin} -S -s packingType=grid_ccsds $base.tmp $base.cum
+    cat $base.cum >> $base
+    rm $base.tmp $base.cum
     mv $base $filesfc
 
   fi
@@ -208,7 +221,7 @@ for step in $steplist; do
   if [ ! -f $filesoil ]; then
     for itry in `seq 1 10`; do
       for v in $varsoil; do
-        V=`echo $v | tr  '[:lower:]' '[:upper:]'`
+        V=$v #`echo $v | tr  '[:lower:]' '[:upper:]'`
           destfile=${filepref}${fcdate}_${step}_${V}.grib2.bz2
           vtmp=`echo $v| sed -e 's/.*_w_so/w_so/' -e 's/.*_t_so/t_so/'`  ##Cut soil level from varname
           [ -f $destfile ] || echo "curl -s -f --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 70 $urlpref/$vtmp/$destfile -o $destfile.tmp && mv ${destfile}.tmp ${destfile}"
@@ -240,8 +253,10 @@ echo Cleanup..
 cd $scriptdir
 rm -rvf $scratchdir
 echo Done! 
-
-
+#https://opendata.dwd.de/weather/nwp/icon-d2/grib/03/z0/
+#icon-d2_germany_regular-lat-lon_single-level_2021021103_001_2d_z0.grib2.bz2
+#icon-d2_germany_regular-lat-lon_single-level_2021021103_045_2d_z0.grib2.bz2
 exit 0
 #
-
+icon-d2_germany_regular-lat-lon_soil-level_2021021103_001_0_t_so.grib2.bz2
+icon-d2_germany_regular-lat-lon_soil-level_2021021103_001_2d_0_t_so.grib2.bz2
